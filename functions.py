9d35e3a9d43ae4f3e7e86d45e52f27bfe7f9c7dc
jkung <jeremykung@gmail.com>
2024-03-28 12:56:13 +0200

    added functions

---
+++ functions.py
@@ -0,0 +1,103 @@
+# 0. What are Functions? ✅
+# 1. Defining ✅
+# 2. Calling ✅
+# 3. Parameters & Arguments ✅
+# 4. Default Values
+# 5. Return Values
+
+# Cooking steak
+# 1. Kill cow
+# 2. Transport cow
+# 3. Cut the cow
+# 4. Cook the cow
+# 5. Add seasoning
+
+# Defining a function
+def makeSteak():
+    print("kill cow")
+    print("transport cow")
+    print("cut cow")
+    print("cook")
+    print("season")
+
+# Call a function (actually do it)
+# makeSteak()
+
+# Function with a PARAMETER (topping)
+def makeRice(topping):
+    print("I made rice with", topping)
+
+# Call the function with an ARGUMENT (chicken)
+makeRice("chicken")
+# Call the function with an ARGUMENT (cheeseburgers)
+makeRice("cheeseburgers")
+
+def add_drink(drink):
+    print("I ordered my meal with", drink)
+
+add_drink("coke")
+add_drink("Philip drinking milk")
+
+# Create a function that accepts your lucky number as a parameter. Call the function a few times with different numbers as arguments.
+
+# def luckyNumber(number):
+#     print("this is my lucky number:", number)
+
+# luckyNumber(3)
+# luckyNumber(12)
+
+# Functions with multiple parameters/arguments
+def makeSandwich(meat, vegetable, sauce):
+    print("I made a", meat, vegetable, "sandwich with", sauce)
+
+makeSandwich("ham", "ketchup", "lettuce")
+
+# Make a function that accepts two numbers as parameters
+# Add the numbers together
+# Print the sum
+
+
+
+
+
+
+
+
+
+
+
+
+# # define the function
+# def makeCookies():
+#     print('get eggs, flour, water')
+#     print('mix all ingredients together')
+#     print('heat oven and bake cookies')
+
+# # call the function (actually doing it)
+# # makeCookies()
+# # makeCookies()
+
+# # function with a parameter (amount)
+# def makeCake(amount):
+#     print('get ingredients')
+#     print('mix and bake')
+#     print('deccorate')
+#     print('cakes made:', amount)
+
+# # makeCake(4)
+# # makeCake(2)
+
+# # function with a parameter (number)
+# def doubleNumber(number):
+#     print(number * 2)
+
+# # function call with an argument (3)
+# doubleNumber(3)
+# # function call with an argument (10)
+# doubleNumber(10)
+
+# def multiply(a, b):
+#     answer = a * b
+#     print(answer)
+
+# multiply(6, 8)
