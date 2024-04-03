ae2099f2b0fcc249166b8f373bdaaad830274e43
jkung <jeremykung@gmail.com>
2024-04-03 15:02:50 +0300

    added return & default fn

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 # 1. Defining ✅
 # 2. Calling ✅
 # 3. Parameters & Arguments ✅
-# 4. Default Values
+# 4. Default Values ✅
 # 5. Return Values
 
 # Cooking steak
@@ -57,47 +57,55 @@ makeSandwich("ham", "ketchup", "lettuce")
 # Print the sum
 
 
+# Default Parameters
+def salute(name="Mr. Kung"):
+    print("goodbye", name)
+
+salute("Klara")
+
+salute()
+
+# Make a function with two parameters (person1, person2)
+# Each parameter should have a default value
+# The function will say hi to both people
+# Call it with arguments
+# Call it without arguments
+# Call it with one argument
+
+def greet(person1="Mr. Kung", person2="Venla"):
+    print("hi", person1, "and", person2)
+
+greet("Dominika", "Maria F")
+greet()
+greet("Olivers")
+
+# Function with return value ("purple")
+def giveMeColour():
+    return "purple"
+
+
+maxCatcher = "giveMeColour()"
+print(maxCatcher)
+
+def add_one(number):
+    number_plus_one = number + 1
+    return number_plus_one
+
+result = add_one(1)
+print(result)
+
+# Create a function that returns a number minus 2
+# return that number
+# catch it in a variable
+# print the variable
+
+def minusTwo(number):
+    answer = number - 2
+    return answer
+
+result = minusTwo(7)
+print(result)
 
 
 
 
-
-
-
-
-
-
-# # define the function
-# def makeCookies():
-#     print('get eggs, flour, water')
-#     print('mix all ingredients together')
-#     print('heat oven and bake cookies')
-
-# # call the function (actually doing it)
-# # makeCookies()
-# # makeCookies()
-
-# # function with a parameter (amount)
-# def makeCake(amount):
-#     print('get ingredients')
-#     print('mix and bake')
-#     print('deccorate')
-#     print('cakes made:', amount)
-
-# # makeCake(4)
-# # makeCake(2)
-
-# # function with a parameter (number)
-# def doubleNumber(number):
-#     print(number * 2)
-
-# # function call with an argument (3)
-# doubleNumber(3)
-# # function call with an argument (10)
-# doubleNumber(10)
-
-# def multiply(a, b):
-#     answer = a * b
-#     print(answer)
-
-# multiply(6, 8)
