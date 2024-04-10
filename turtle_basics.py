c85333b4f5ecfc8b1c4b94fd25ef51bb3483c98e
jkung <jeremykung@gmail.com>
2024-04-10 14:32:34 +0300

    added turtle lesson

---
+++ turtle_basics.py
@@ -0,0 +1,32 @@
+# we are importing a library of functions for drawing
+from turtle import *
+
+# draw 100 steps forward
+forward(100)
+
+# rotate the pen left 90 degrees
+left(90)
+
+# draw 200 steps forward
+forward(200)
+
+# draw a circle with a radius of 30
+circle(30)
+
+# move the pen to (0,0)
+goto(0,0)
+
+# lifts up the pencil
+penup()
+
+# move the pen to (-200,-200)
+goto(-200,-200)
+
+# puts the pencil back down
+pendown()
+
+# draw 50 steps forward
+forward(50)
+
+# keeps the window open after the drawing is done
+done()
