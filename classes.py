7086ac8caa97941939859aceb41471f2fe3353d4
jkung <jeremykung@gmail.com>
2024-05-08 15:00:41 +0300

    added classes lesson

--- classes.py
+++ classes.py
@@ -1,40 +1,103 @@
-# What is a class?
-#   - constructor function: def __init__()
-#   - default values
-#   - default methods
+# What is a class? ✅
+#   - constructor function: def __init__() ✅
+#   - default values ✅
+#   - default methods ✅
 
-# Instantiation
-#   - accessing object attributes
-#   - accessing object methods
+# Instantiation ✅
+#   - accessing object attributes ✅
+#   - accessing object methods ✅
 
-# Examples of classes and objects
-#   - turtle
-#   - random
+# Examples of classes and objects ✅
+#   - turtle ✅
 
-class Animal:
-    def __init__(self, name, color, sound, extinct):
-        self.name = name
+
+# Create a blueprint for house (Class)
+
+class House:
+    # constructor function
+    def __init__(self, roof, numWindows, color):
+        self.roof = roof
+        self.numWindows = numWindows
         self.color = color
-        self.sound = sound
-        self.extinct = extinct
 
-    def describe(self):
-        print(self.name, "is", self.color, "and makes a", self.sound, "sound")
+    # custom functions
+    def heatHouse(self, degrees):
+        print("we are heating the house to", degrees)
 
-    def findPet(self):
-        if (self.extinct):
-            print("can't be a pet, they are exctinct")
-        else:
-            print("you went to the pet store and got one!")
 
-spot = Animal("dog", "white and brown", "woof", False)
 
-spot.describe()
-spot.findPet()
+# Create objects using the Class (Instantiate)
 
-mammoth = Animal("wooly mammoth", "brown", "who knows", True)
+vladsHouse = House("cube", 69, "light blue")
+vladsHouse.heatHouse(25)
 
-mammoth.findPet()
+domsHouse = House("circle", 7, "orange")
+jinheHouse = House("triangle", 5, "black")
+mrkungHouse = House("no roof", 0, "clay")
+
+print(domsHouse.color)          # orange
+print(domsHouse.numWindows)     # 7
+
+
+
+# Car Example
+
+class Car:
+    def __init__(self, spoiler, doors, color):
+        self.spoiler = spoiler
+        self.doors = doors
+        self.color = color
+
+    def honk(self):
+        print("your car honked")
+
+    def describeCar(self):
+        print("your car is", self.color, "and has", self.doors, "doors" )
+
+maxCar = Car(True, 2, "Red")
+alexCar = Car(True, 2, "Black")
+klaraCar = Car(False, 7, "Purple")
+
+klaraCar.describeCar()
+maxCar.honk()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Animal:
+#     def __init__(self, name, color, sound, extinct):
+#         self.name = name
+#         self.color = color
+#         self.sound = sound
+#         self.extinct = extinct
+
+#     def describe(self):
+#         print(self.name, "is", self.color, "and makes a", self.sound, "sound")
+
+#     def findPet(self):
+#         if (self.extinct):
+#             print("can't be a pet, they are exctinct")
+#         else:
+#             print("you went to the pet store and got one!")
+
+# spot = Animal("dog", "white and brown", "woof", False)
+
+# spot.describe()
+# spot.findPet()
+
+# mammoth = Animal("wooly mammoth", "brown", "who knows", True)
+
+# mammoth.findPet()
 
 
 
